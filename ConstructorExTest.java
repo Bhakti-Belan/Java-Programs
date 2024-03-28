68641a68ebbfff2e4ce0be50bddda101c57139f5
Bhakti-Belan <164329732+Bhakti-Belan@users.noreply.github.com>
2024-03-28 21:42:16 +0530

    Add files via upload

---
+++ ConstructorExTest.java
@@ -0,0 +1,11 @@
+public class ConstructorExTest
+{
+public static void main(String args[])
+{
+MyRectangle m1=new MyRectangle();
+System.out.println("Area Of Rectangle: "+m1.area());
+MyRectangle m2=new MyRectangle(10,20);
+System.out.println("Area Of Rectangle: "+m2.area());
+}
+}
+
---
+++ MyRectangle.java
@@ -0,0 +1,17 @@
+public class MyRectangle
+{
+int length,breadth;
+public MyRectangle()
+{
+int length=0,breadth=0;
+}
+public MyRectangle(int length,int breadth)
+{
+this.length=length;
+this.breadth=breadth;
+}
+int area()
+{
+return length*breadth;
+}
+}
